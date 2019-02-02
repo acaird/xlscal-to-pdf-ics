0db66270aa27b39dc2f11f82ef79b8470822d8a0
Andrew Caird <acaird@gmail.com>
2019-02-02 18:16:23 -0500

    receive the upload

--- app/main.py
+++ app/main.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World from Flask"
+def upload():
+    page = """<html>
+   <body>
+      <form action = "/uploader" method = "POST" 
+         enctype = "multipart/form-data">
+         <input type = "file" name = "file" />
+         <input type = "submit"/>
+      </form>
+   </body>
+</html>"""
+    return page
+
+
+@app.route("/uploader", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        f = request.files["file"]
+        f.save(secure_filename(f.filename))
+        return 'file "{}" uploaded successfully'.format(f.filename)
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host="0.0.0.0", debug=True, port=8080)
