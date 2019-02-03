44cfa537e7c009af3d31319204fc302fe610518a
Andrew Caird <acaird@gmail.com>
2019-02-02 22:52:54 -0500

    Added ability to load csv and xlsx and display data
     - add two `pip install` commands to the Dockerfile
    
     - process csv or xlsx files and print pre-formatted output

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.7
 
+RUN pip install xlrd
+RUN pip install python-dateutil
+
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
+import os
+import csv
+import json
+import xlrd
+from dateutil.parser import parse
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -20,10 +26,55 @@ def upload():
 
 @app.route("/uploader", methods=["GET", "POST"])
 def upload_file():
+    can_process = [".csv", ".xlsx"]
     if request.method == "POST":
         f = request.files["file"]
-        f.save(secure_filename(f.filename))
-        return 'file "{}" uploaded successfully'.format(f.filename)
+        sfname = secure_filename(f.filename)
+        a = f.save(sfname)
+
+        filename, fileext = os.path.splitext(sfname)
+        if fileext not in can_process:
+            message = (
+                'Files of type "{}" cannot be processed, please '
+                "upload a file of one of these types: {}".format(
+                    fileext, ", ".join(can_process)
+                )
+            )
+            return message
+
+        if fileext == ".csv":
+            ret = parse_csv(sfname)
+        if fileext == ".xlsx":
+            ret = parse_xlsx(sfname)
+
+        return "<pre>{}</pre>".format(json.dumps(ret, indent=4))
+
+
+def parse_csv(filename):
+    l = []
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            line[0] = "{}".format(parse(line[0]))
+            l.append(line)
+
+    return l
+
+
+def parse_xlsx(filename):
+    workbook = xlrd.open_workbook(filename)
+    sheet = workbook.sheet_by_index(0)
+
+    l = []
+
+    for rowx in range(sheet.nrows):
+        cols = sheet.row_values(rowx)
+        cols[0] = "{}".format(
+            datetime(*xlrd.xldate_as_tuple(cols[0], workbook.datemode))
+        )
+        l.append(cols)
+
+    return l
 
 
 if __name__ == "__main__":
