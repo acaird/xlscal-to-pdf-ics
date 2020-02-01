249ee2c9f67e2c5971754e027d0a206ddfa8351c
Andrew Caird <acaird@gmail.com>
2020-02-01 15:53:07 -0500

    Moved from a bunch of crazy functions to an object
    Still needs a lot more clean-up, but this is a lot better

--- app/main.py
+++ app/main.py
@@ -30,6 +30,259 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
+class EventList:
+    def __init__(self, file_, filename, http_request):
+        self.file_ = file_
+        self.filename = filename
+        self.request = http_request
+        self.can_process = [".csv", ".xlsx"]
+        self.fileroot, self.fileext = os.path.splitext(self.filename)
+        self.events = None
+        self.headers = ["time", "description", "endtime", "location"]
+
+    def _validate(self):
+        """Validate a file based on its extension
+
+        Returns
+        -------
+        None
+            if the file is valid
+        str
+            containing the error if the file is invalid
+
+        """
+        if self.fileext not in self.can_process:
+            message = (
+                'Files of type "{}" cannot be processed, please '
+                "upload a file of one of these types: {}".format(
+                    fileext, ", ".join(self.can_process)
+                )
+            )
+            return message
+
+        return None
+
+    def _parse_csv(self):
+        """Parse an uploaded CSV file
+
+        Returns
+        -------
+        list
+            datetime.datetime, str, ...
+        """
+        csv_lines = []
+        with open(self.filename, "r") as f:
+            try:
+                reader = csv.reader(f)
+                for line in reader:
+                    line[0] = parse(line[0])  # dateutil.parser
+                    csv_lines.append(line)
+            except ValueError as e:
+                logging.debug(e)
+                csv_lines = None
+
+        logging.debug(csv_lines)
+        return csv_lines
+
+    def _parse_xlsx(self):
+        """Parse an uploaded XLSX file
+
+        Returns
+        -------
+        list
+            datetime.datetime, str, ...
+        """
+        try:
+            workbook = xlrd.open_workbook(self.filename)
+        except xlrd.biffh.XLRDError as e:
+            logging.debug(e)
+            return None
+
+        sheet = workbook.sheet_by_index(0)
+
+        xls_line = []
+
+        for rowx in range(sheet.nrows):
+            cols = sheet.row_values(rowx)
+            cols[0] = datetime(*xlrd.xldate_as_tuple(cols[0], workbook.datemode))
+
+            xls_line.append(cols)
+
+        return xls_line
+
+    def _structure_events(self, events):
+        """Take events in lists and return them with some structure
+
+        The structure is a list of objects that look like: {"time":
+        datetime.datetime, "description": str, "endtime":
+        datetime.datetime, "location": str}
+
+        Parameters
+        ----------
+        events: list
+           list of lists, each sub-list is the ordered list of details
+           assuming the order: time, description, endtime, location
+
+        Returns
+        -------
+        list
+           list of objects describing each event using the format
+           described above
+
+        """
+        events.sort()
+        events_list = []
+
+        for event in events:
+            event_obj = dict.fromkeys(self.headers)
+            for index, field in enumerate(event):
+                event_obj[self.headers[index]] = field
+            logging.debug(event_obj)
+            events_list.append(event_obj)
+
+        return events_list
+
+    def _fill_cal(self, cal, mon, yr):
+        for row, _ in enumerate(cal):
+            for day, _ in enumerate(cal[row]):
+                if not isinstance(cal[row][day], int) or int(cal[row][day]) < 1:
+                    if cal[row][day] == 0:
+                        cal[row][day] = ""
+                    continue
+                date = "{}/{}/{}".format(mon, cal[row][day], yr)
+                date = datetime.strptime(date, "%m/%d/%Y")
+                for event in self.events:
+                    if event["time"].date() == date.date():
+                        if event["time"].time() != "00:00:00":
+                            cal[row][day] = str(cal[row][day]) + "\n{}\n{}".format(
+                                datetime.strftime(event["time"], "%H:%M"),
+                                event["description"],
+                            )
+                        else:
+                            cal[row][day] = str(cal[row][day]) + "\n{}".format(
+                                event["description"]
+                            )
+
+                        logging.debug(
+                            "fill_cal : event : {}".format(event["description"])
+                        )
+                    else:
+                        logging.debug(f"No event on {date}")
+
+        return cal
+
+    def loadfile(self):
+        """Loads data into the EventList object
+
+        """
+        ret = self._validate()
+        if ret:
+            return ret
+
+        if self.fileext == ".csv":
+            events = self._parse_csv()
+            if events is None:
+                return "The .csv file was badly formatted, check it again try again."
+        if self.fileext == ".xlsx":
+            events = self._parse_xlsx(sfname)
+            if events is None:
+                return "The .xlsx file was badly formatted, check it again try again."
+
+        self.events = self._structure_events(events)
+
+        return None
+
+    def make_pdf_cals(self, return_flask_resp=False):
+        logging.debug(f"make_pdf_cals: Events: {self.events}")
+        buf = BytesIO()
+        stylesheet = getSampleStyleSheet()
+        doc = SimpleDocTemplate(buf, pagesize=letter)
+        doc.pagesize = landscape(letter)
+        elements = []
+        months = set([d["time"].month for d in self.events])
+        years = set([d["time"].year for d in self.events])
+        logging.debug(f"make_pdf_cals: Months: {months}")
+        logging.debug(f"make_pdf_cals: Years: {years}")
+
+        for year in years:
+            for month in months:
+                elements.append(
+                    Paragraph(
+                        "{} {}".format(calendar.month_name[month], year),
+                        stylesheet["Title"],
+                    )
+                )
+                cal = [["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]]
+                cal.extend(calendar.monthcalendar(year, month))
+                cal = self._fill_cal(cal, month, year)
+                table = Table(cal, 7 * [1.25 * inch], len(cal) * [0.8 * inch])
+                table.setStyle(
+                    TableStyle(
+                        [
+                            ("FONT", (0, 0), (-1, -1), "Helvetica"),
+                            ("FONT", (0, 0), (-1, 0), "Helvetica-Bold"),
+                            ("FONTSIZE", (0, 0), (-1, -1), 8),
+                            ("INNERGRID", (0, 0), (-1, -1), 0.25, colors.black),
+                            ("BOX", (0, 0), (-1, -1), 0.25, colors.green),
+                            ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+                            ("VALIGN", (0, 0), (-1, -1), "TOP"),
+                        ]
+                    )
+                )
+
+                elements.append(table)
+                elements.append(PageBreak())
+
+        doc.build(elements)
+
+        return buf.getvalue()
+
+    def make_ics(self):
+        ical = Calendar()
+
+        for e in self.events:
+            event = Event()
+            event.add("summary", e["description"])
+            logging.debug(
+                "Date: {}  Time: {}".format(e["time"].date(), e["time"].time())
+            )
+            logging.debug(f"All: {e}")
+            # if the time is midnight, assume it's an all day event
+            if e["time"].time() == "00:00:00":
+                event.add("dtstart", e["time"].date())
+            else:
+                event.add("dtstart", e["time"])
+
+            ical.add_component(event)
+
+        return ical.to_ical(ical)
+
+    def make_zipfile(self):
+        """Make a zip file with the PDF and ICS files in it
+
+        Returns
+        -------
+        flask.Response
+        """
+        memory_file = BytesIO()
+        zip_file = ZipFile(memory_file, "w")
+
+        pdfcals = self.make_pdf_cals()
+        if pdfcals:
+            zip_file.writestr(f"calendar-{self.fileroot}.pdf", pdfcals)
+
+        ics = self.make_ics()
+        zip_file.writestr(f"calendar-{self.fileroot}.ics", ics.decode("ascii"))
+
+        zip_file.close()
+        resp = Response(memory_file.getvalue())
+        resp.headers["Content-Disposition"] = "attachment; filename={}".format(
+            f"cals-{self.fileroot}.zip"
+        )
+        resp.headers["Content-Type"] = "application/zip"
+        return resp
+
+
 @app.route("/")
 def upload():
     return render_template("upload_page.html")
@@ -38,6 +291,7 @@ def upload():
 @app.route("/uploader", methods=["GET", "POST"])
 def upload_file():
     can_process = [".csv", ".xlsx"]
+
     if request.method == "POST":
         if "file" not in request.files:
             return (
@@ -48,191 +302,11 @@ def upload_file():
         sfname = secure_filename(f.filename)
         f.save(sfname)
 
-        filename, fileext = os.path.splitext(sfname)
-        if fileext not in can_process:
-            message = (
-                'Files of type "{}" cannot be processed, please '
-                "upload a file of one of these types: {}".format(
-                    fileext, ", ".join(can_process)
-                )
-            )
-            return message
-
-        if fileext == ".csv":
-            ret = parse_csv(sfname)
-            if ret is None:
-                return "The .csv file was badly formatted, check it again try again."
-        if fileext == ".xlsx":
-            ret = parse_xlsx(sfname)
-            if ret is None:
-                return "The .xlsx file was badly formatted, check it again try again."
-
-        # Why do I hate myself.  Why couldn't this have been sensible
-        # like: evtdict[evt[0]] = {"title": evt[1], "location":
-        # evt[2], "notes": evt[3]} also why couldn't this have been
-        # all in one place so it wasn't scattered hither and yon.  fuck
-        evtdict = {}
-        for evt in ret:
-            evtdict[evt[0]] = evt[1]
-
-        memory_file = BytesIO()
-        zip_file = ZipFile(memory_file, "w")
-
-        pdfcals = make_pdf_cals(evtdict)
-        zip_file.writestr("calendar.pdf", pdfcals)
-
-        ics = make_ics(evtdict)
-        zip_file.writestr("calendar.ics", ics.decode("ascii"))
-
-        zip_file.close()
-        resp = Response(memory_file.getvalue())
-        resp.headers["Content-Disposition"] = "attachment; filename={}".format(
-            "cals.zip"
-        )
-        resp.headers["Content-Type"] = "application/zip"
-        return resp
-
-
-def parse_csv(filename):
-    csv_line = []
-    with open(filename, "r") as f:
-        try:
-            reader = csv.reader(f)
-            for line in reader:
-                line[0] = parse(line[0])  # dateutil.parser
-                csv_line.append(line)
-        except ValueError:
-            return None
-    logging.debug(csv_line)
-    return csv_line
-
-
-def parse_xlsx(filename):
-    try:
-        workbook = xlrd.open_workbook(filename)
-    except xlrd.biffh.XLRDError:
-        return None
-
-    sheet = workbook.sheet_by_index(0)
-
-    xls_line = []
-
-    for rowx in range(sheet.nrows):
-        cols = sheet.row_values(rowx)
-        cols[0] = datetime(*xlrd.xldate_as_tuple(cols[0], workbook.datemode))
-
-        xls_line.append(cols)
-
-    return xls_line
-
-
-def make_pdf_cals(events):
-    buf = BytesIO()
-    stylesheet = getSampleStyleSheet()
-    doc = SimpleDocTemplate(buf, pagesize=letter)
-    doc.pagesize = landscape(letter)
-    elements = []
-    months = set([d.month for d in events])
-    years = set([d.year for d in events])
-    logging.debug(f"make_pdf_cals: Events: {events}")
-    logging.debug(f"make_pdf_cals: Months: {months}")
-    logging.debug(f"make_pdf_cals: Years: {years}")
-
-    for year in years:
-        for month in months:
-            elements.append(
-                Paragraph(
-                    "{} {}".format(calendar.month_name[month], year),
-                    stylesheet["Title"],
-                )
-            )
-            cal = [["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]]
-            cal.extend(calendar.monthcalendar(year, month))
-            cal = fill_cal(cal, month, year, events)
-            table = Table(cal, 7 * [1.25 * inch], len(cal) * [0.8 * inch])
-            table.setStyle(
-                TableStyle(
-                    [
-                        ("FONT", (0, 0), (-1, -1), "Helvetica"),
-                        ("FONT", (0, 0), (-1, 0), "Helvetica-Bold"),
-                        ("FONTSIZE", (0, 0), (-1, -1), 8),
-                        ("INNERGRID", (0, 0), (-1, -1), 0.25, colors.black),
-                        ("BOX", (0, 0), (-1, -1), 0.25, colors.green),
-                        ("ALIGN", (0, 0), (-1, -1), "LEFT"),
-                        ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                    ]
-                )
-            )
-
-            elements.append(table)
-            elements.append(PageBreak())
-
-    doc.build(elements)
-
-    pdf = buf.getvalue()
-
-    return pdf
-
-
-def evt_to_lists(events):
-    evt = {}
-    for e in events:
-        date = datetime.strptime(
-            datetime.strftime(e, "%Y-%m-%d 00:00"), "%Y-%m-%d %H:%M"
-        )
-        if date not in evt:
-            evt[date] = []
-
-        time = datetime.strftime(e, "%H:%M")
-        if time == "00:00":
-            time = ""
-        evt[date].append("{} {}".format(time, events[e]))
-
-    return evt
-
-
-def fill_cal(cal, mon, yr, events):
-    evts = evt_to_lists(events)
-    logging.debug("Evts: {}".format(evts))
-    for row, _ in enumerate(cal):
-        for day, _ in enumerate(cal[row]):
-            if not isinstance(cal[row][day], int) or int(cal[row][day]) < 1:
-                if cal[row][day] == 0:
-                    cal[row][day] = ""
-                continue
-            date = "{}/{}/{}".format(mon, cal[row][day], yr)
-            date = datetime.strptime(date, "%m/%d/%Y")
-            # this doesn't work because `date` is no longer just
-            # m/d/y, but also has time now; I need to remove the time
-            # part in the events dict before trying to reference it, I
-            # think
-            if date in evts:
-                for evt in evts[date]:
-                    cal[row][day] = str(cal[row][day]) + "\n{}".format(evt)
-                    logging.debug(f"fill_cal : evt : {evt}")
-            else:
-                logging.debug(f"No event on {date}")
-
-    return cal
-
-
-def make_ics(events):
-    ical = Calendar()
-
-    for m in sorted(events):
-        event = Event()
-        event.add("summary", events[m])
-        logging.debug("Date: {}  Time: {}".format(m.date(), m.time()))
-        logging.debug(f"All: {m}")
-        # if the time is midnight, assume it's an all day event
-        if m.time() == "00:00:00":
-            event.add("dtstart", m.date())
-        else:
-            event.add("dtstart", m)
-
-        ical.add_component(event)
-
-    return ical.to_ical(ical)
+        events = EventList(f, sfname, request)
+        load_status = events.loadfile()
+        if load_status:
+            return load_status, 400
+        return events.make_zipfile()
 
 
 def _get_env_val(env_var, default=None):
