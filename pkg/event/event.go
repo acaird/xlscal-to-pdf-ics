33dddc3e810ff8354129f3b6ef345696bebd83f6
Andrew Caird <acaird@gmail.com>
2023-03-05 18:01:12 -0500

    make events more robust, get the weeks in a month right

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -2,7 +2,9 @@ package event
 
 import (
 	"fmt"
+	"path/filepath"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/araddon/dateparse"
@@ -27,37 +29,49 @@ func (e *Events) Print() {
 	}
 }
 
-func (e *Events) Parse(records [][]string) {
+func (e *Events) ParseCSV(records [][]string) error {
 	// Parse takes a slice of slices, where the inner slice is of
 	// the format: startTime, title, endTime, location, and sets
 	// `e` to a list of Event structs
 	for _, record := range records {
 		var event Event
 		var err error
+		recordSize := len(record)
+		if recordSize == 0 {
+			continue
+		}
 		event.startDate, err = dateparse.ParseLocal(record[0])
 		if err != nil {
-			panic(err)
+			return err
 		}
-		event.title = record[1]
-		var endTime time.Time
-		if record[2] != "" {
-			endTime, err = time.Parse("15:04", record[2])
-			if err != nil {
-				panic(err)
+		if recordSize > 1 {
+			event.title = record[1]
+		}
+		var end time.Time
+		if recordSize > 2 {
+			var endTime time.Time
+			if record[2] != "" {
+				end, err = dateparse.ParseLocal(record[2])
+				if err != nil {
+					return err
+				}
+			} else {
+				endTime, _ = time.Parse("15:04", "23:59")
+				end = time.Date(event.startDate.Year(), event.startDate.Month(), event.startDate.Day(),
+					endTime.Hour(), endTime.Minute(), 0, 0, event.startDate.Location())
 			}
-		} else {
-			endTime, _ = time.Parse("15:04", "23:59")
-		}
 
-		end := time.Date(event.startDate.Year(), event.startDate.Month(), event.startDate.Day(),
-			endTime.Hour(), endTime.Minute(), 0, 0, event.startDate.Location())
-		event.endDate = end
-		if err != nil {
-			panic(err)
+			event.endDate = end
+			if err != nil {
+				return err
+			}
 		}
-		event.location = record[3]
+		if recordSize > 3 {
+			event.location = record[3]
+		}
 		*e = append(*e, event)
 	}
+	return nil
 }
 
 func (e Events) MakeICS() string {
@@ -83,8 +97,12 @@ func (e Events) Months() []time.Time {
 	// Months returns a sorted list of year/month data
 	eventMonths := make(map[time.Time]bool)
 	for _, evt := range e {
-		eventMonths[time.Date(evt.endDate.Year(), evt.endDate.Month(), 1, 0, 0, 0, 0, evt.endDate.Location())] = true
-		eventMonths[time.Date(evt.startDate.Year(), evt.startDate.Month(), 1, 0, 0, 0, 0, evt.startDate.Location())] = true
+		if evt.endDate.Year() > 1900 {
+			eventMonths[time.Date(evt.endDate.Year(), evt.endDate.Month(), 1, 0, 0, 0, 0, evt.endDate.Location())] = true
+		}
+		if evt.startDate.Year() > 1900 {
+			eventMonths[time.Date(evt.startDate.Year(), evt.startDate.Month(), 1, 0, 0, 0, 0, evt.startDate.Location())] = true
+		}
 	}
 	var months []time.Time
 	for month := range eventMonths {
@@ -96,7 +114,7 @@ func (e Events) Months() []time.Time {
 	return months
 }
 
-func (e Events) PdfCal() {
+func (e Events) PdfCal(filename string) error {
 	type Pos struct {
 		x float64
 		y float64
@@ -121,8 +139,8 @@ func (e Events) PdfCal() {
 		// make the right number of rows in the calendar
 		monthStart := now.With(calMonth).BeginningOfMonth()
 		monthEnd := now.With(calMonth).EndOfMonth()
-		_, endWeek := monthEnd.ISOWeek()
-		_, startWeek := monthStart.ISOWeek()
+		_, endWeek := monthEnd.AddDate(0, 0, 1).ISOWeek() // add a day because ISOWeek starts on Monday, and we want Sunday
+		_, startWeek := monthStart.AddDate(0, 0, 1).ISOWeek()
 		weeksInMonth := float64(endWeek - startWeek + 1)
 		if weeksInMonth < 0 {
 			weeksInMonth = 52 + weeksInMonth - 1
@@ -157,21 +175,25 @@ func (e Events) PdfCal() {
 		}
 		// write the events into the boxes
 		pdf.SetFont("Arial", "", float64(eventSize))
-		for _, ee := range e {
-			if !ee.InMonth(calMonth) {
+		for _, event := range e {
+			if !event.InMonth(calMonth) {
 				continue
 			}
-			d := ee.startDate.Day()
+			d := event.startDate.Day()
 			textX := dateCoords[d].x + 3
 			textY := dateCoords[d].y + 5
-			for _, line := range pdf.SplitText(fmt.Sprintf("%s %s",
-				ee.startDate.Format("3:04 PM"), ee.title),
-				0.95*sizeX) {
+			description := ""
+			if event.startDate.Hour() == 0 && event.startDate.Minute() == 0 {
+				description = event.title
+			} else {
+				description = fmt.Sprintf("%s %s", event.startDate.Format("3:04 PM"), event.title)
+			}
+			for _, line := range pdf.SplitText(description, 0.95*sizeX) {
 				pdf.Text(textX, textY, line)
 				textY += float64(eventSize) - 3.5
 			}
 			pdf.SetFont("Arial", "I", float64(locationSize))
-			for _, line := range pdf.SplitText(ee.location, 0.95*sizeX) {
+			for _, line := range pdf.SplitText(event.location, 0.95*sizeX) {
 				pdf.Text(textX, textY, line)
 				textY += float64(locationSize) - 2.0
 			}
@@ -183,10 +205,12 @@ func (e Events) PdfCal() {
 		pdf.Line(0, pageHeight, pageWidth, pageHeight) // draw across the "bottom" of the landscape page
 		// write the file
 	}
-	err := pdf.OutputFileAndClose("hello.pdf")
+	filename = strings.TrimSuffix(filename, filepath.Ext(filename))
+	err := pdf.OutputFileAndClose(filename + ".pdf")
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
 func (e Event) InMonth(month time.Time) bool {
