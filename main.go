46b8496f3038e8daa8b50d131834b80a76d76170
Andrew Caird <acaird@gmail.com>
2023-03-05 15:36:31 -0500

    produce nicely formatted PDF calendars from events

--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"os"
-	"time"
-
-	"github.com/jinzhu/now"
-	"github.com/jung-kurt/gofpdf"
 
 	"github.com/acaird/xlscal-to-pdf-ics/pkg/event"
 )
@@ -24,70 +19,6 @@ func main() {
 	events.Parse(records)
 	// i := events.MakeICS()
 	// fmt.Println(i)
-	pdfCal()
-}
-
-func pdfCal() {
-	type Pos struct {
-		x float64
-		y float64
-	}
-	// can we make a pdf calendar page?
-	weekdays := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
-	pdf := gofpdf.New("L", "mm", "letter", "")
-	pageWidth, pageHeight := pdf.GetPageSize()
-	pdf.AddPage()
-	// write the header
-	pdf.SetFont("Arial", "B", 16)
-	n := time.Now()
-	n = n.AddDate(0, -1, 0)
-	pdf.Cell(40, 10, fmt.Sprintf("%s %d", n.Month(), n.Year()))
-	// make the grid
-	pdf.SetLineWidth(0.3)
-	pdf.Line(40, 10, 41, 10)
-	startX := 10.0
-	startY := 30.0
-	maxX := pageWidth - startX
-	maxY := pageHeight - startY
-	sizeX := (maxX - startX) / 7.0
-	sizeY := (maxY - startY) / 5.0
-	pdf.SetDrawColor(190, 190, 190) // gray
-	for i := 0.0; i <= 5.0; i++ {
-		pdf.Line(startX, startY+(i*sizeY), maxX, startY+(i*sizeY))
-	}
-	pdf.SetFont("Arial", "", 8)
-	for i := 0.0; i <= 7.0; i++ {
-		if i < 7.0 {
-			pdf.Text(startX+(i*sizeX)+2.0, startY-2.0, fmt.Sprintf("%s", weekdays[int(i)]))
-		}
-		pdf.Line(startX+(i*sizeX), startY, startX+(i*sizeX), maxY)
-	}
-	// write in the dates
-	monthStart := now.With(n).BeginningOfMonth()
-	monthEnd := now.With(n).EndOfMonth()
-	xPos := startX + float64(int(monthStart.Weekday()))*sizeX
-	yPos := startY + 5
-	x := make(map[int]Pos)
-	for day := 1; day <= monthEnd.Day(); day++ {
-		x[day] = Pos{
-			x: xPos,
-			y: yPos,
-		}
-		pdf.Text(xPos+2, yPos, fmt.Sprintf("%d", day))
-		xPos += sizeX
-		if xPos > maxX {
-			xPos = startX
-			yPos += sizeY
-		}
-	}
-	// draw a cool border at the top and bottom
-	pdf.SetLineWidth(10)
-	pdf.Line(0, 0, pageWidth, 0)                   // draw across the "top" of the landscape page
-	pdf.Line(0, pageHeight, pageWidth, pageHeight) // draw across the "bottom" of the landscape page
-	// write the file
-	err := pdf.OutputFileAndClose("hello.pdf")
-	if err != nil {
-		panic(err)
-	}
-
+	events.PdfCal()
+	// fmt.Println(months)
 }
--- pkg/event/event.go
+++ pkg/event/event.go
@@ -8,6 +8,8 @@ import (
 	"github.com/araddon/dateparse"
 	ics "github.com/arran4/golang-ical"
 	"github.com/google/uuid"
+	"github.com/jinzhu/now"
+	"github.com/jung-kurt/gofpdf"
 )
 
 type Event struct {
@@ -65,7 +67,7 @@ func (e Events) MakeICS() string {
 	cal.SetMethod(ics.MethodAdd)
 	for _, evt := range e {
 		uuid := uuid.NewString()
-		event := cal.AddEvent(fmt.Sprintf(uuid))
+		event := cal.AddEvent(uuid)
 		event.SetCreatedTime(time.Now())
 		event.SetDtStampTime(time.Now())
 		event.SetModifiedAt(time.Now())
@@ -92,4 +94,101 @@ func (e Events) Months() []time.Time {
 		return months[i].Before(months[j])
 	})
 	return months
+}
+
+func (e Events) PdfCal() {
+	type Pos struct {
+		x float64
+		y float64
+	}
+	dateSize := 8
+	eventSize := 7
+	locationSize := 6
+	// make a pdf calendar page
+	weekdays := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+	pdf := gofpdf.New("L", "mm", "letter", "")
+	pageWidth, pageHeight := pdf.GetPageSize()
+	startX, startY := 10.0, 30.0
+	maxX, maxY := pageWidth-startX, pageHeight-startY
+	sizeX := (maxX - startX) / 7.0
+	for _, calMonth := range e.Months() {
+		pdf.AddPage()
+		// write the header
+		pdf.SetFont("Arial", "B", 16)
+		pdf.Cell(40, 10, fmt.Sprintf("%s %d", calMonth.Month(), calMonth.Year()))
+		pdf.SetLineWidth(0.3)
+		pdf.SetDrawColor(190, 190, 190) // gray
+		// make the right number of rows in the calendar
+		monthStart := now.With(calMonth).BeginningOfMonth()
+		monthEnd := now.With(calMonth).EndOfMonth()
+		_, endWeek := monthEnd.ISOWeek()
+		_, startWeek := monthStart.ISOWeek()
+		weeksInMonth := float64(endWeek - startWeek + 1)
+		if weeksInMonth < 0 {
+			weeksInMonth = 52 + weeksInMonth - 1
+		}
+		sizeY := (maxY - startY) / weeksInMonth
+		// make the grid with the weekday headings
+		for i := 0.0; i <= weeksInMonth; i++ {
+			pdf.Line(startX, startY+(i*sizeY), maxX, startY+(i*sizeY))
+		}
+		pdf.SetFont("Arial", "", float64(dateSize))
+		for i := 0.0; i <= 7.0; i++ {
+			if i < 7.0 {
+				pdf.Text(startX+(i*sizeX)+2.0, startY-2.0, weekdays[int(i)])
+			}
+			pdf.Line(startX+(i*sizeX), startY, startX+(i*sizeX), maxY)
+		}
+		// write in the dates and save note locations for later
+		xPos := startX + float64(int(monthStart.Weekday()))*sizeX
+		yPos := startY + 5.0
+		dateCoords := make(map[int]Pos)
+		for day := 1; day <= monthEnd.Day(); day++ {
+			dateCoords[day] = Pos{
+				x: xPos,
+				y: yPos,
+			}
+			pdf.Text(xPos+2, yPos, fmt.Sprintf("%d", day)) // put the date in the box
+			xPos += sizeX
+			if xPos > 0.9*maxX {
+				xPos = startX
+				yPos += sizeY
+			}
+		}
+		// write the events into the boxes
+		pdf.SetFont("Arial", "", float64(eventSize))
+		for _, ee := range e {
+			if !ee.InMonth(calMonth) {
+				continue
+			}
+			d := ee.startDate.Day()
+			textX := dateCoords[d].x + 3
+			textY := dateCoords[d].y + 5
+			for _, line := range pdf.SplitText(fmt.Sprintf("%s %s",
+				ee.startDate.Format("3:04 PM"), ee.title),
+				0.95*sizeX) {
+				pdf.Text(textX, textY, line)
+				textY += float64(eventSize) - 3.5
+			}
+			pdf.SetFont("Arial", "I", float64(locationSize))
+			for _, line := range pdf.SplitText(ee.location, 0.95*sizeX) {
+				pdf.Text(textX, textY, line)
+				textY += float64(locationSize) - 2.0
+			}
+			pdf.SetFont("Arial", "", float64(eventSize))
+		}
+		// draw a cool border at the top and bottom
+		pdf.SetLineWidth(10)
+		pdf.Line(0, 0, pageWidth, 0)                   // draw across the "top" of the landscape page
+		pdf.Line(0, pageHeight, pageWidth, pageHeight) // draw across the "bottom" of the landscape page
+		// write the file
+	}
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (e Event) InMonth(month time.Time) bool {
+	return month.Month() == e.startDate.Month()
 }
